ea3429bc818a8503287256c4177a469a07a3e7cf
Mathias Geat <mathias@ailoo.net>
2010-04-05 17:34:10 +0000

    update widget to WP 2.8+ widget API
    git-svn-id: http://svn.wp-plugins.org/nextgen-gallery-sidebar-widget/trunk@225560 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ngg-sidebar-widget.php
+++ ngg-sidebar-widget.php
@@ -14,10 +14,165 @@ Author URI: http://ailoo.net/
  *
  * 0.2.2            Add gallery_thumbnail option to select thumbnail image (preview, first, random)
  */
+ 
+add_action('widgets_init', create_function('', 'return register_widget("NextGEN_Gallery_Sidebar_Widget");'));
+ 
+class NextGEN_Gallery_Sidebar_Widget extends WP_Widget
+{
+    function NextGEN_Gallery_Sidebar_Widget()
+    {
+		$widget_ops = array('classname' => 'ngg-sidebar-widget', 'description' => 'A widget to show random galleries with preview image.');
+		$this->WP_Widget('ngg-sidebar-widget', 'NextGEN Gallery Sidebar Widget', $widget_ops);
+    }
+    
+	function widget($args, $instance)
+    {
+        global $wpdb;
+        extract($args);
+        $title = apply_filters('widget_title', $instance['title']);
+   
+        switch($instance['gallery_order']) {
+            case 'added_desc':
+                $order = 'gid DESC';
+                break;
+            case 'added_asc':
+                $order = 'gid ASC';
+                break;
+            default:
+                $order = 'RAND()';
+                break;
+        }
+                
+        $results = $wpdb->get_results("SELECT * FROM $wpdb->nggallery ORDER BY " . $order . " LIMIT " . $instance['max_galleries']);
+        if(is_array($results) && count($results) > 0) {
+            $galleries = array();
+            foreach($results as $result) {
+                if($wpdb->get_var("SELECT COUNT(pid) FROM $wpdb->nggpictures WHERE galleryid = '" . $result->gid . "'") > 0) {
+                    if($instance['gallery_thumbnail'] == 'preview' && (int)$result->previewpic > 0) {
+                        // ok
+                    } elseif($instance['gallery_thumbnail'] == 'random') {
+                        $result->previewpic = $wpdb->get_var("SELECT pid FROM $wpdb->nggpictures WHERE galleryid = '" . $result->gid . "' ORDER BY RAND() LIMIT 1");
+                    } else {
+                        // else take the first image
+                        $result->previewpic = $wpdb->get_var("SELECT pid FROM $wpdb->nggpictures WHERE galleryid = '" . $result->gid . "' ORDER BY sortorder ASC, pid ASC LIMIT 1");
+                    }
+                    
+                    $galleries[] = $result;
+                }
+            }
+            
+            if(count($galleries) > 0) {
+                $title = $instance['title'];
+                if(isset($instance['default_link']) && trim($instance['default_link']) != '') {
+                    $title = '<a href="' . get_permalink($instance['default_link']) . '">' . $instance['title'] . '</a>';
+                }
+            
+                $output = "\n";
+                $output .= $args['before_widget'] . "\n";
+                $output .= $args['before_title'] . $title . $args['after_title'] . "\n";
+                
+                foreach($galleries as $gallery) {
+                    $imagerow = $wpdb->get_row("SELECT * FROM $wpdb->nggpictures WHERE pid = '" . $gallery->previewpic . "'");
+                    foreach($gallery as $key => $value) {
+                        $imagerow->$key = $value;
+                    }
+                    
+                    $image = new nggImage($imagerow);
+                    
+                    if($gallery->pageid > 0) {
+                        $gallery_link = get_permalink($gallery->pageid);
+                    } elseif(!empty($instance['default_link'])) {
+                        $gallery_link = get_permalink($instance['default_link']);
+                    } else {
+                        $gallery_link = get_permalink(1);
+                    }
+                    
+                    $output .= '<a href="' . $gallery_link . '">';
+                    
+                    if(function_exists('getphpthumburl') && trim($instance['autothumb_params']) != '') {
+                        $output .= '<img src="' . getphpthumburl($image->imageURL, $instance['autothumb_params']) . '" title="' . $gallery->title . '" alt="' . $gallery->title . '" />';
+                    } else {
+                        $output .= '<img src="' . $image->thumbURL . '" title="' . $gallery->title . '" alt="' . $gallery->title . '" width="' . $instance['output_width'] . '" height="' . $instance['output_height'] . '" />';               
+                    }
+                    
+                    $output .= '</a>';
+                }
+                
+                $output .= '<br style="clear: both" />';
+                $output .= "\n" . $args['after_widget'] . "\n";
+                echo $output;
+            }
+        }
+	}
 
-add_action('plugins_loaded', 'init_ngg_sidebar_gallery_widget');
+	function form($instance)
+    {
+		$instance = wp_parse_args((array) $instance, array( 
+            'title' => 'Galleries',
+            'max_galleries' => 6,
+            'gallery_order' => 'random',
+            'gallery_thumbnail' => 'first',
+            'autothumb_params' => '',
+            'output_width' => 100,
+            'output_height' => 75,
+            'default_link' => 1
+        ));
+    ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>">Widget Title</label>
+            <input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $instance['title'] ?>" />
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('max_galleries'); ?>">Maximum Galleries</label>
+            <input type="text" id="<?php echo $this->get_field_id('max_galleries'); ?>" name="<?php echo $this->get_field_name('max_galleries'); ?>" value="<?php echo $instance['max_galleries'] ?>" />
+        <p>
+            <label for="<?php echo $this->get_field_id('gallery_order'); ?>">Gallery Order</label><br />
+            <select id="<?php echo $this->get_field_name('gallery_order'); ?>" name="<?php echo $this->get_field_name('gallery_order'); ?>">';
+                <option value="random" <?php echo ($instance['gallery_order'] == 'random') ? ' selected="selected"' : ''; ?>>Random</option>
+                <option value="added_asc" <?php echo ($instance['gallery_order'] == 'added_asc') ? ' selected="selected"' : ''; ?>>Date added ASC</option>
+                <option value="added_desc" <?php echo ($instance['gallery_order'] == 'added_desc') ? ' selected="selected"' : ''; ?>>Date added DESC</option>
+            </select>
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('gallery_thumbnail'); ?>">Gallery thumbnail image</label><br />
+            <select id="<?php echo $this->get_field_name('gallery_thumbnail'); ?>" name="<?php echo $this->get_field_name('gallery_thumbnail'); ?>">';
+              <option value="preview" <?php echo ($instance['gallery_thumbnail'] == 'preview') ? ' selected="selected"' : ''; ?>>Gallery Preview (set in NGG)</option>
+              <option value="first" <?php echo ($instance['gallery_thumbnail'] == 'first') ? ' selected="selected"' : ''; ?>>First</option>
+              <option value="random" <?php echo ($instance['gallery_thumbnail'] == 'random') ? ' selected="selected"' : ''; ?>>Random</option>
+            </select>
+        </p>
+        <p>
+              <label for="<?php echo $this->get_field_id('autothumb_params'); ?>">Autothumb Parameters (if installed)</label>
+              <input type="text" id="<?php echo $this->get_field_id('autothumb_params'); ?>" name="<?php echo $this->get_field_name('autothumb_params'); ?>" value="<?php echo $instance['autothumb_params'] ?>" />
+        </p>    
+        <p>
+              <label for="<?php echo $this->get_field_id('output_width'); ?>">Output width</label>
+              <input type="text" id="<?php echo $this->get_field_id('output_width'); ?>" name="<?php echo $this->get_field_name('output_width'); ?>" value="<?php echo $instance['output_width'] ?>" />
+        </p>    
+        <p>
+              <label for="<?php echo $this->get_field_id('output_height'); ?>">Output height</label>
+              <input type="text" id="<?php echo $this->get_field_id('output_height'); ?>" name="<?php echo $this->get_field_name('output_height'); ?>" value="<?php echo $instance['output_height'] ?>" />
+        </p>
+        <p>
+              <label for="<?php echo $this->get_field_id('default_link'); ?>">Default Link Id (galleries without image page)</label>
+              <input type="text" id="<?php echo $this->get_field_id('default_link'); ?>" name="<?php echo $this->get_field_name('default_link'); ?>" value="<?php echo $instance['default_link'] ?>" />
+        </p>
+    <?php
+	}
 
-function init_ngg_sidebar_gallery_widget()
+	function update($new_instance, $old_instance)
+    {
+        $new_instance['title'] = htmlspecialchars($new_instance['title']);
+        return $new_instance;
+	}
+} 
+
+
+ 
+
+//add_action('plugins_loaded', 'init_ngg_sidebar_gallery_widget');
+
+/*function init_ngg_sidebar_gallery_widget()
 {
     // check if NextGEN Gallery is installed
     if (class_exists('nggLoader')) {
@@ -223,4 +378,4 @@ function ngg_sidebar_gallery_widget_get_options()
 	}
 
     return $options;
-}
+}*/
